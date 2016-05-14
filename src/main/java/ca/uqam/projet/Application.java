369fa7e4cd5e747a5dfc90a111efa21beeb01581
gcoll_000 <gcoll_000@192.168.0.100>
2016-05-14 17:43:09 -0400

    La récupération de l'horaire se fait à intervalles réguliers (au 5 seconde)

--- src/main/java/ca/uqam/projet/Application.java
+++ src/main/java/ca/uqam/projet/Application.java
@@ -1,10 +1,10 @@
 package ca.uqam.projet;
 
 import ca.uqam.projet.repositories.FoodTruckList;
-import ca.uqam.projet.resources.FoodTruck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,11 +12,10 @@ import org.springframework.web.client.RestTemplate;
 public class Application {
 
     public static void main(String[] args) {
-        // SpringApplication.run(Application.class, args);
-        RestTemplate restTemplate = new RestTemplate();
-        FoodTruckList foodTruckList = restTemplate.getForObject("http://camionderue.com/donneesouvertes/geojson", FoodTruckList.class);
-
-        System.out.println(foodTruckList);
+        SpringApplication.run(Application.class, args);
+        //showList(foodTruckList);
+        
+       
 
         /*
          // SpringApplication.run(Application.class, args);
@@ -29,5 +28,11 @@ public class Application {
          System.out.println("--------------------HEEEEEEEEEEEEEEEERE----------------------------3");
          System.out.println(foodTruckList);
          */
+    }
+@Scheduled(fixedDelay=5000)
+    public void showList() { 
+        RestTemplate restTemplate = new RestTemplate();
+        FoodTruckList foodTruckList = restTemplate.getForObject("http://camionderue.com/donneesouvertes/geojson", FoodTruckList.class);
+        System.out.println(foodTruckList);
     }
 }
