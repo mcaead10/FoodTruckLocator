c1f0db0f3eaf36d0e75ad7cc877bb330c8e9ca37
mcaead10 <m.a.allard10@gmail.com>
2016-06-16 16:19:40 -0400

    Requete de recherche fonctionne dans le html

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,15 +1,17 @@
 var start = document.getElementById("startDate");
 var end = document.getElementById("endDate");
 var btn = document.getElementById("btn-valid");
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function(e) {
+  e.preventDefault();
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
                 if (xhttp.readyState === 4 && xhttp.status === 200) {
                         var json = JSON.parse(xhttp.responseText);
-                        alert("Voici le nomber de camion"+ json.itemb.length );
+                        alert("Voici le nomber de camion" + json.length );
                 }
         };
-        xhttp.open("GET", "http://localhost:8080/horaires-camions?du="+start+"&au="+end, true);
+        console.log( "/horaires-camions?du="+start.value+"&au="+end.value);
+        xhttp.open("GET", "/horaires-camions?du="+start.value+"&au="+end.value, true);
         xhttp.send();
 });
 
