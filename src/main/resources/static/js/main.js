6768017d65b1188404f94acec49dc861f0c902b2
mcaead10 <m.a.allard10@gmail.com>
2016-06-16 15:11:20 -0400

    Debut de la page html

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
   console.log('Ça marche')
 })
+
+function calc() {
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
+
+  var sum = parseInt(a) + parseInt(b)
+  alert(parseInt(a) + " + " + parseInt(b) +" = " + sum);
+
+  return false;
+}
