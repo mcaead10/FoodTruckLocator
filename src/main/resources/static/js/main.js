c5b7ecd3b12ec3986c75b10f60a81ae986a70a15
ba991181 <ba991181@C32654.ens.uqam.ca>
2016-06-16 16:40:32 -0400

    A5 is done

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,21 @@
-var start = document.getElementById("startDate");
-var end = document.getElementById("endDate");
+var start = document.getElementById("startDate").value;
+var end = document.getElementById("endDate").value;
 var btn = document.getElementById("btn-valid");
-btn.addEventListener("click", function(e) {
-  e.preventDefault();
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-                if (xhttp.readyState === 4 && xhttp.status === 200) {
-                        var json = JSON.parse(xhttp.responseText);
-                        alert("Voici le nomber de camion" + json.length );
-                }
-        };
-        console.log( "/horaires-camions?du="+start.value+"&au="+end.value);
-        xhttp.open("GET", "/horaires-camions?du="+start.value+"&au="+end.value, true);
-        xhttp.send();
+btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (!start || !end)
+        {
+            alert("Format de date incorect");
+        } else {
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+                var json = JSON.parse(xhttp.responseText);
+                alert("Voici le nomber de camion" + json.length);
+            }
+        }
+    };
+    xhttp.open("GET", "/horaires-camions?du=" + start + "&au=" + end, true);
+    xhttp.send();
 });
 
