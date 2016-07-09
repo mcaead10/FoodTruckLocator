cb3e1150e7a27f884c95d0d26bb19c4023124819
gcoll_000 <gcoll_000@192.168.0.101>
2016-07-09 03:00:17 -0400

    Bixi charger lorsqu'on clic sur un food truc

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,16 +9,25 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-var markers = new Array();
+var markersFD = new Array();
+var markersB = new Array();
+var markersAV = new Array();
 
 var foodTruckIcon = L.icon({
-        iconUrl: 'foodtruck.png',
-        iconSize:     [40, 50],
-        iconAnchor:   [20, 25],
-        popupAnchor:  [0, -10]
+    iconUrl: 'foodtruck.png',
+    iconSize: [40, 50],
+    iconAnchor: [20, 25],
+    popupAnchor: [0, -10]
 });
 
-document.getElementById("btn-valid").addEventListener("click", function (e) {
+var bixiIcon = L.icon({
+    iconUrl: 'bixi.png',
+    iconSize: [40, 50],
+    iconAnchor: [20, 25],
+    popupAnchor: [0, -10]
+});
+
+document.getElementById("btn-valid").addEventListener("click", function(e) {
     e.preventDefault();
     var start = document.getElementById("startDate").value;
     var end = document.getElementById("endDate").value;
@@ -30,10 +39,10 @@ document.getElementById("btn-valid").addEventListener("click", function (e) {
         return;
     }
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
+    xhttp.onreadystatechange = function() {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
             var json = JSON.parse(xhttp.responseText);
-            markerDelete();
+            markerDeleteAll();
             for (var iter = 0; iter < json.length; iter++) {
                 var marker = L.marker([json[iter].geometry.coordinates[1], json[iter].geometry.coordinates[0]],
                         {icon: foodTruckIcon})
@@ -44,8 +53,7 @@ document.getElementById("btn-valid").addEventListener("click", function (e) {
                                 '<br> Date: ' + json[iter].properties.Date +
                                 '<br> Heure de début: ' + json[iter].properties.Heure_debut +
                                 '<br> Heure de fin: ' + json[iter].properties.Heure_fin)
-                        .openPopup();
-                markers.push(marker);
+                markersFD.push(marker);
             }
         }
     };
@@ -53,15 +61,45 @@ document.getElementById("btn-valid").addEventListener("click", function (e) {
     xhttp.send();
 });
 
-function addVelo(e){
+function addVelo(e) {
+    markerDeleteVelo();
     mymap.setView(e.latlng, 16);
-    
-  //  xhttp.open("GET", "/horaires-camions?du=" + start + "&au=" + end, true);
-  // xhttp.send();
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            var json = JSON.parse(xhttp.responseText);
+            for (var iter = 0; iter < json.length; iter++) {
+                if (json[iter].ouvert) {
+                    var marker = L.marker([json[iter].latitude, json[iter].longitude])
+                            .addTo(mymap)
+                            .bindPopup('Lieu: ' + json[iter].name +
+                                    '<br> Emplacements Disponibles: ' + json[iter].emplacementDisponible +
+                                    '<br> Vélo disponibles: ' + json[iter].veloDisponible);
+                    markersB.push(marker);
+                }
+            }
+        }
+    };
+    xhttp.open("GET", "/bixi?longitude=" + e.latlng.lng + "&latitude=" + e.latlng.lat, true);
+    xhttp.send();
 }
 
-function markerDelete() {
-    for (i = 0; i < markers.length; i++) {
-        mymap.removeLayer(markers[i]);
+function markerDeleteAll() {
+    markerDeleteFD();
+    markerDeleteVelo();
+}
+
+function markerDeleteFD() {
+    for (i = 0; i < markersFD.length; i++) {
+        mymap.removeLayer(markersFD[i]);
+    }
+}
+function markerDeleteVelo() {
+    for (i = 0; i < markersB.length; i++) {
+        mymap.removeLayer(markersB[i]);
+    }
+    for (i = 0; i < markersAV.length; i++) {
+        mymap.removeLayer(markersAV[i]);
     }
 }
